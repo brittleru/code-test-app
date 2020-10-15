9a382a7086a124dbab58f4d71ab2b4c452c3e6f2
Sebastian Mocanu <diemetal851@gmail.com>
2020-10-16 00:14:44 +0300

    made date format to human readable date and posted it on page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider, Query } from "react-apollo";
 import gql from "graphql-tag";
+import moment from "moment";
 
 // Connect my site to GraphQL/ FakerQL
 const client = new ApolloClient({
@@ -39,17 +40,41 @@ client.query({
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Data Histrogram</h1>
         </header>
 
+        <Query query={postsQuery}>
+          {({loading, error, data}) => {
+            if (loading) {
+              return <div>Loading...</div>
+            }
+            if (error) {
+              return <div>Error {error.toString()}</div>
+            }
 
+            if(data) {
+              return  (
+                <div>
+                  {
+                    data.allPosts.map((post) => {
+                      let theDate = moment(new Date(parseFloat(post.["createdAt"]))).format("MMM Do YY");
+                      return <div key={post.id}>{`Date: ${theDate}`}</div>
+                    })
+                  }
+                </div>
+              )
+            }
+          }}
+        </Query>
 
         <footer>
             by Mocanu Sebastian
         </footer>
       </div>
+      </ApolloProvider>
     );
   }
 

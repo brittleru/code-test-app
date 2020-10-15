167361cd736481ff05e9e261347722b63b1063a0
Sebastian Mocanu <diemetal851@gmail.com>
2020-10-15 23:46:43 +0300

    get query from graphQL with Apollo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React, {Component} from 'react';
-
 import './App.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
+import gql from "graphql-tag";
+
+// Connect my site to GraphQL/ FakerQL
+const client = new ApolloClient({
+  uri: `https://fakerql.stephix.uk/graphql`
+});
+
+// My Query of posts
+const postsQuery = gql `
+{
+  allPosts(count: 5) {
+    id
+    title
+    body
+    published
+    createdAt
+    author {
+      id
+      firstName
+      lastName
+      avatar
+    }
+  }
+}
+`;
+
+// Running query
+client.query({
+    query: postsQuery
+  }).then(res => {
+    console.log(res);
+  });
+
 
 class App extends Component {
   render() {
@@ -10,7 +44,7 @@ class App extends Component {
           <h1 className="App-title">Data Histrogram</h1>
         </header>
 
-        
+
 
         <footer>
             by Mocanu Sebastian
